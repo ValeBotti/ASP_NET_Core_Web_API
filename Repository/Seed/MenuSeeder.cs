821c6f03eace81fe7f0dde66e37734e50054212b
valeb <vale.botti@outlook.com>
2026-09-12 13:09:23 +0200

    Fix: seeder relative path

--- Repository/Seed/MenuSeeder.cs
+++ Repository/Seed/MenuSeeder.cs
@@ -1,33 +1,21 @@
 using System.Text.Json;
-
 public static class MenuSeeder
 {
-    public static void Seed(AppDbContext db)
+    public static void Seed(AppDbContext db, IWebHostEnvironment env)
     {
-        
         if (db.Menus.Any())
             return;
 
-        var jsonPath = @"Repository\Seed\menus.json";
-
+        var jsonPath = Path.Combine(env.ContentRootPath, "Repository", "Seed", "menus.json");
         var json = File.ReadAllText(jsonPath);
 
         var items = JsonSerializer.Deserialize<List<MenuSeedDto>>(json);
 
-        if (items is null || items.Count == 0)
-        {
-            Console.WriteLine("MenuSeeder: nessun elemento trovato nel JSON.");
-            return;
-        }
-
         foreach (var item in items)
         {
-
-            var basePath = Directory.GetCurrentDirectory();
-            var imagePath = Path.Combine(basePath, "Repository", "Seed", "images", item.ImageFile);
+            var imagePath = Path.Combine(env.WebRootPath, "images", item.ImageFile);
 
             var bytes = File.ReadAllBytes(imagePath);
-
             var base64 = Convert.ToBase64String(bytes);
 
             var menu = new Menu
